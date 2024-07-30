f5734227b6ca55bee61bb71bb92480fac5ba08c7
Jonathan ARNAULT <jonathan@marmelab.com>
2024-07-30 11:10:35 +0200

    Feat(signup): Add user signup support

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,7 +1,14 @@
 import { supabaseDataProvider } from 'ra-supabase';
 
-import { DataProvider, GetListParams, withLifecycleCallbacks } from 'react-admin';
+import {
+    DataProvider,
+    GetListParams,
+    Identifier,
+    withLifecycleCallbacks,
+} from 'react-admin';
+import { getActivityLog } from './dataProvider/activity';
 import { supabase } from './supabase';
+import { Deal, SignUpData } from './types';
 
 if (import.meta.env.VITE_SUPABASE_URL === undefined) {
     throw new Error('Please set the VITE_SUPABASE_URL environment variable');
@@ -20,13 +27,96 @@ const baseDataProvider = supabaseDataProvider({
 
 const dataProviderWithCustomMethods = {
     ...baseDataProvider,
+    signUp: async ({ email, password, first_name, last_name }: SignUpData) => {
+        const response = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+                data: {
+                    first_name,
+                    last_name,
+                },
+            },
+        });
+        if (response.error) {
+            console.error('signUp.error', response.error);
+            throw new Error('Failed to create account');
+        }
+
+        return {
+            email,
+            password,
+        };
+    },
+    transferAdministratorRole: async (from: Identifier, to: Identifier) => {
+        const { data: sales } = await baseDataProvider.getList('sales', {
+            filter: { id: [from, to] },
+            pagination: { page: 1, perPage: 2 },
+            sort: { field: 'name', order: 'ASC' },
+        });
+
+        const fromSale = sales.find(sale => sale.id === from);
+        const toSale = sales.find(sale => sale.id === to);
+
+        if (!fromSale || !toSale) {
+            return null;
+        }
+
+        await baseDataProvider.update('sales', {
+            id: to,
+            data: {
+                administrator: true,
+            },
+            previousData: toSale,
+        });
+
+        const updatedUser = await baseDataProvider.update('sales', {
+            id: from,
+            data: {
+                administrator: false,
+            },
+            previousData: fromSale,
+        });
+        return updatedUser.data;
+    },
+    unarchiveDeal: async (deal: Deal) => {
+        // get all deals where stage is the same as the deal to unarchive
+        const { data: deals } = await baseDataProvider.getList<Deal>('deals', {
+            filter: { stage: deal.stage },
+            pagination: { page: 1, perPage: 1000 },
+            sort: { field: 'index', order: 'ASC' },
+        });
+
+        // set index for each deal starting from 1, if the deal to unarchive is found, set its index to the last one
+        const updatedDeals = deals.map((d, index) => ({
+            ...d,
+            index: d.id === deal.id ? 0 : index + 1,
+            archived_at: d.id === deal.id ? null : d.archived_at,
+        }));
+
+        return await Promise.all(
+            updatedDeals.map(updatedDeal =>
+                baseDataProvider.update('deals', {
+                    id: updatedDeal.id,
+                    data: updatedDeal,
+                    previousData: deals.find(d => d.id === updatedDeal.id),
+                })
+            )
+        );
+    },
+    getActivityLog: async (companyId?: Identifier) => {
+        return getActivityLog(baseDataProvider, companyId);
+    },
     async isInitialized() {
-        return false;
+        const { data } = await supabase
+            .from('init_state')
+            .select('is_initialized');
+
+        return data?.at(0)?.is_initialized > 0;
     },
 } satisfies DataProvider;
 
 export type CustomDataProvider = typeof dataProviderWithCustomMethods;
-
 
 export const dataProvider = withLifecycleCallbacks(
     dataProviderWithCustomMethods,
