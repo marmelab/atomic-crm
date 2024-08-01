e81e5ecffe22fccebdaaaa9eebbaa42ad3c8e192
Anthony Rimet <anthony.rimet03@gmail.com>
2024-08-01 21:32:15 +0200

    WIP(crm): Update Activity and Dashboard

--- src/activity/ActivityLog.tsx
+++ src/activity/ActivityLog.tsx
@@ -1,15 +1,22 @@
 import { Alert, Divider, Skeleton, Stack } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { Identifier, useDataProvider } from 'react-admin';
+
 import { CustomDataProvider } from '../dataProvider';
+import { ActivityLogContext } from './ActivityLogContext';
 import { ActivityLogIterator } from './ActivityLogIterator';
 
 type ActivityLogProps = {
     companyId?: Identifier;
     pageSize?: number;
+    context?: 'company' | 'contact' | 'deal' | 'all';
 };
 
-export function ActivityLog({ companyId, pageSize = 20 }: ActivityLogProps) {
+export function ActivityLog({
+    companyId,
+    pageSize = 20,
+    context = 'all',
+}: ActivityLogProps) {
     const dataProvider = useDataProvider<CustomDataProvider>();
     const { data, isPending, error } = useQuery({
         queryKey: ['activityLog', companyId],
@@ -18,7 +25,7 @@ export function ActivityLog({ companyId, pageSize = 20 }: ActivityLogProps) {
 
     if (isPending) {
         return (
-            <Stack p={2}>
+            <Stack mt={0.5}>
                 {Array.from({ length: 5 }).map((_, index) => (
                     <Stack spacing={2} sx={{ mt: 1 }} key={index}>
                         <Stack
@@ -42,8 +49,12 @@ export function ActivityLog({ companyId, pageSize = 20 }: ActivityLogProps) {
     }
 
     if (error) {
-        return <Alert severity="error">Failed to load acticity log</Alert>;
+        return <Alert severity="error">Failed to load activity log</Alert>;
     }
 
-    return <ActivityLogIterator activities={data} pageSize={pageSize} />;
+    return (
+        <ActivityLogContext.Provider value={context}>
+            <ActivityLogIterator activities={data} pageSize={pageSize} />
+        </ActivityLogContext.Provider>
+    );
 }
