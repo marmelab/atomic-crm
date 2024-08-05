cc658df1452c93d6efd548e1616e7ecf2459eeaa
Jean-Baptiste Kaiser <jb@marmelab.com>
2024-08-05 18:53:09 +0200

    init a mailgun edge function

---
+++ supabase/functions/mailgun/index.ts
@@ -0,0 +1,30 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
+
+console.log('Hello from Functions!');
+
+Deno.serve(async req => {
+    const { name } = await req.json();
+    const data = {
+        message: `Hello ${name}!`,
+    };
+
+    return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/mailgun' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
