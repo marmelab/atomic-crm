e090fe388af939f723f832211f7ea512ec3043a2
Jonathan ARNAULT <jonathan@marmelab.com>
2024-08-05 09:46:06 +0200

    Feat(ops): Add CI/CD pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		visualizer({
-			open: process.env.NODE_ENV !== "CI",
-			filename: "./dist/stats.html",
-		}),
-	],
-	define: {
-		"process.env": process.env,
-	},
-	base: "./",
-	esbuild: {
-		keepNames: true,
-	},
-	build: {
-		sourcemap: true,
-	},
-	resolve: {
-		preserveSymlinks: true,
-	},
+    plugins: [
+        react(),
+        visualizer({
+            open: process.env.NODE_ENV !== 'CI',
+            filename: './dist/stats.html',
+        }),
+    ],
+    define: {
+        'process.env': process.env,
+    },
+    base: './',
+    esbuild: {
+        keepNames: true,
+    },
+    build: {
+        sourcemap: true,
+    },
+    resolve: {
+        preserveSymlinks: true,
+    },
 });
