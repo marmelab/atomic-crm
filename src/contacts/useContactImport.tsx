f618108b22b1dd3ba9809be525f19b3dc0f273a5
François Zaninotto <fzaninotto@gmail.com>
2024-08-23 17:54:56 +0200

    Add timeout on avatar fetch to avoid breaking the import on rate limited domains

--- src/contacts/useContactImport.tsx
+++ src/contacts/useContactImport.tsx
@@ -103,6 +103,7 @@ export function useContactImport() {
                         tags: tagNames,
                         linkedin_url,
                     }) => {
+                        const singleEmail = email.split(',')[0];
                         const company = companyName?.trim()
                             ? companies.get(companyName.trim())
                             : undefined;
@@ -116,7 +117,7 @@ export function useContactImport() {
                                 last_name,
                                 gender,
                                 title,
-                                email,
+                                email: singleEmail,
                                 phone_1_number,
                                 phone_1_type,
                                 phone_2_number,
