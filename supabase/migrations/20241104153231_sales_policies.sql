4a1570945c07fe76d2ca3e7884ae3082f8ae83a7
React-Admin CI <react-admin@marmelab.com>
2024-11-05 10:48:59 +0100

    Remove sales update RLS policy

--- supabase/migrations/20241104153231_sales_policies.sql
+++ supabase/migrations/20241104153231_sales_policies.sql
@@ -35,16 +35,3 @@ as permissive
 for insert
 to authenticated
 with check (private.is_admin());
-
-
-create policy "Enable update for authenticated users only"
-on "public"."sales"
-as permissive
-for update
-to authenticated
--- Users can only update their own records, unless they are administrators
--- This is the *Can I see the current record?* check
-using (((( SELECT auth.uid() AS uid) = user_id) OR private.is_admin()))
--- Users can only update their own records and only if they don't change their administrator column, unless they are administrators
--- This is the *Can I set those new values?* check
-with check ((((( SELECT auth.uid() AS uid) = user_id) AND (administrator = false)) OR private.is_admin()));
