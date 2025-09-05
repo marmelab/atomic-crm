2d4dfa58908f52c879796aaffd93d7c2a47398a3
Matthieu Hochlander <matthieu@marmelab.com>
2025-09-05 11:25:09 +0200

    Remove biome-related comments.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-// biome-ignore lint/style/noNonNullAssertion: #root element is guaranteed to exist
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
