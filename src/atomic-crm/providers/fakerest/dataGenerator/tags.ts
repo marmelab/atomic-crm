01cb042a1b8f177cca8078e090fe5a19bd229fab
fzaninotto <fzaninotto@gmail.com>
2025-09-16 11:15:44 +0200

    Change tags color

--- src/atomic-crm/providers/fakerest/dataGenerator/tags.ts
+++ src/atomic-crm/providers/fakerest/dataGenerator/tags.ts
@@ -1,12 +1,12 @@
 import type { Db } from "./types";
 
 const tags = [
-  { id: 0, name: "football-fan", color: "#424D5B" },
-  { id: 1, name: "holiday-card", color: "#535F7D" },
-  { id: 2, name: "influencer", color: "#C1050D" },
-  { id: 3, name: "manager", color: "#6C5B61" },
-  { id: 4, name: "musician", color: "#494582" },
-  { id: 5, name: "vip", color: "#765A3A" },
+  { id: 0, name: "football-fan", color: "#eddcd2" },
+  { id: 1, name: "holiday-card", color: "#fff1e6" },
+  { id: 2, name: "influencer", color: "#fde2e4" },
+  { id: 3, name: "manager", color: "#fad2e1" },
+  { id: 4, name: "musician", color: "#c5dedd" },
+  { id: 5, name: "vip", color: "#dbe7e4" },
 ];
 
 export const generateTags = (_: Db) => {
