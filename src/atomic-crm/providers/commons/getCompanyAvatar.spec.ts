94fd536ea00eb4268958d01277faecceb9066efd
fzaninotto <fzaninotto@gmail.com>
2025-09-17 14:13:03 +0200

    Fix unit test

--- src/atomic-crm/providers/commons/getCompanyAvatar.spec.ts
+++ src/atomic-crm/providers/commons/getCompanyAvatar.spec.ts
@@ -6,12 +6,12 @@ import type { Company } from "../../types";
 import { getCompanyAvatar } from "./getCompanyAvatar";
 
 it("should return favicon URL if website url exist", async () => {
-  const website = "https://gravatar.com";
+  const website = "https://example.com";
   const record: Partial<Company> = { website };
 
   const avatarUrl = await getCompanyAvatar(record);
   expect(avatarUrl).toStrictEqual({
-    src: "https://gravatar.com/favicon.ico",
+    src: "https://favicon.show/example.com",
     title: "Company favicon",
   });
 });
