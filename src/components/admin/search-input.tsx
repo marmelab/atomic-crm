5b28f825584c3b0c27da5c911b2834a1287b8293
mpsalunggg <putrasatria893@gmail.com>
2025-09-29 15:42:21 +0700

    chore: adjust widht container input from w-fit to w-full

--- src/components/admin/search-input.tsx
+++ src/components/admin/search-input.tsx
@@ -14,7 +14,7 @@ export const SearchInput = (inProps: SearchInputProps) => {
   }
 
   return (
-    <div className="flex flex-grow relative mt-auto w-fit">
+    <div className="flex flex-grow relative mt-auto w-full">
       <TextInput
         label={false}
         helperText={false}
