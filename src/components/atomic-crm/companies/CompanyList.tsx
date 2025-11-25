b4e6732dec100882f1a58e80fb715d49ad9693c6
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2025-11-25 17:40:41 +0100

    Improve companies list for mobile

--- src/components/atomic-crm/companies/CompanyList.tsx
+++ src/components/atomic-crm/companies/CompanyList.tsx
@@ -9,9 +9,20 @@ import { TopToolbar } from "../layout/TopToolbar";
 import { CompanyEmpty } from "./CompanyEmpty";
 import { CompanyListFilter } from "./CompanyListFilter";
 import { ImageList } from "./GridList";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { SearchInput, TextField } from "@/components/admin";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+import { Plus } from "lucide-react";
+import { CompanyAvatar } from "./CompanyAvatar";
+import { RecordsIterator } from "ra-core";
+import { Card } from "@/components/ui/card";
+import { useRecordContext } from "ra-core";
+import { useReferenceManyFieldController } from "ra-core";
 
 export const CompanyList = () => {
   const { identity } = useGetIdentity();
+  const isMobile = useIsMobile();
   if (!identity) return null;
   return (
     <List
@@ -20,8 +31,31 @@ export const CompanyList = () => {
       sort={{ field: "name", order: "ASC" }}
       actions={<CompanyListActions />}
       pagination={<ListPagination rowsPerPageOptions={[10, 25, 50, 100]} />}
+      filters={
+        isMobile
+          ? [
+              <SearchInput
+                source="q"
+                alwaysOn
+                className="w-full"
+                placeholder="Name, sector"
+              />,
+            ]
+          : undefined
+      }
     >
       <CompanyListLayout />
+      <Button
+        variant="default"
+        size="icon"
+        className="rounded-full fixed bottom-12 right-12 w-12 h-12"
+        asChild
+      >
+        <Link to="/companies/create">
+          <span className="sr-only">Create new company</span>
+          <Plus />
+        </Link>
+      </Button>
     </List>
   );
 };
@@ -29,26 +63,85 @@ export const CompanyList = () => {
 const CompanyListLayout = () => {
   const { data, isPending, filterValues } = useListContext();
   const hasFilters = filterValues && Object.keys(filterValues).length > 0;
+  const isMobile = useIsMobile();
 
   if (isPending) return null;
   if (!data?.length && !hasFilters) return <CompanyEmpty />;
 
   return (
     <div className="w-full flex flex-row gap-8">
-      <CompanyListFilter />
+      {isMobile ? null : <CompanyListFilter />}
       <div className="flex flex-col flex-1 gap-4">
-        <ImageList />
+        {isMobile ? (
+          <CompanyMobileList />
+        ) : (
+          <ImageList />
+        )}
       </div>
     </div>
   );
 };
 
+const CompanyMobileList = () => (
+  <Card className="p-2">
+    <ul className="flex flex-col gap-4">
+      <RecordsIterator
+        render={(record) => (
+          <li className="flex flex-col">
+            <Link
+              className="flex items-center gap-2"
+              to={`/companies/${record.id}`}
+            >
+              <CompanyAvatar />
+              <div className="flex flex-col grow">
+                <TextField source="name" />
+                <div className="flex items-center justify-between gap-2 text-sm text-gray-400">
+                  <TextField source="sector" />
+                  <div className="flex items-center gap-1">
+                    <DealsCount />
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </li>
+        )}
+      />
+    </ul>
+  </Card>
+);
+/**
+ * Necessary until we have a render prop on ReferenceManyCountBase
+ */
+const DealsCount = () => {
+  const record = useRecordContext();
+  const { isLoading, error, total } = useReferenceManyFieldController({
+    page: 1,
+    perPage: 1,
+    record,
+    reference: "deals",
+    target: "company_id",
+  });
+
+  const body = isLoading
+    ? ""
+    : error
+      ? "error"
+      : `${total} ${total === 1 ? "deal" : "deals"}`;
+  return <span>{body}</span>;
+};
+
 const CompanyListActions = () => {
+  const isMobile = useIsMobile();
+
   return (
     <TopToolbar>
       <SortButton fields={["name", "created_at", "nb_contacts"]} />
-      <ExportButton />
-      <CreateButton label="New Company" />
+      {isMobile ? null : (
+        <>
+          <ExportButton />
+          <CreateButton label="New Company" />
+        </>
+      )}
     </TopToolbar>
   );
 };
