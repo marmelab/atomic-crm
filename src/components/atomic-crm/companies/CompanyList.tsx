85f83be84fb4956563dbf1b9138130c9f6b3fd84
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2025-12-01 10:25:38 +0100

    Improve company list for mobile

--- src/components/atomic-crm/companies/CompanyList.tsx
+++ src/components/atomic-crm/companies/CompanyList.tsx
@@ -1,7 +1,12 @@
-import { RecordsIterator, useGetIdentity, useListContext } from "ra-core";
+import {
+  InfiniteListBase,
+  RecordsIterator,
+  useGetIdentity,
+  useListContext,
+} from "ra-core";
 import { CreateButton } from "@/components/admin/create-button";
 import { ExportButton } from "@/components/admin/export-button";
-import { List } from "@/components/admin/list";
+import { List, ListView } from "@/components/admin/list";
 import { ListPagination } from "@/components/admin/list-pagination";
 import { SortButton } from "@/components/admin/sort-button";
 
@@ -17,11 +22,33 @@ import { Plus } from "lucide-react";
 import { CompanyAvatar } from "./CompanyAvatar";
 import { Card } from "@/components/ui/card";
 import { ReferenceManyCount } from "../misc/ReferenceManyCount";
+import { InfinitePagination } from "../misc/InfinitePagination";
 
 export const CompanyList = () => {
   const { identity } = useGetIdentity();
   const isMobile = useIsMobile();
   if (!identity) return null;
+
+  if (isMobile) {
+    return (
+      <InfiniteListBase>
+        <ListView pagination={<InfinitePagination />} actions={false}>
+          <CompanyListLayout />
+        </ListView>
+        <Button
+          variant="default"
+          size="icon"
+          className="rounded-full fixed bottom-12 right-12 w-12 h-12"
+          asChild
+        >
+          <Link to="/companies/create">
+            <span className="sr-only">Create new company</span>
+            <Plus />
+          </Link>
+        </Button>
+      </InfiniteListBase>
+    );
+  }
   return (
     <List
       title={false}
@@ -29,31 +56,8 @@ export const CompanyList = () => {
       sort={{ field: "name", order: "ASC" }}
       actions={<CompanyListActions />}
       pagination={<ListPagination rowsPerPageOptions={[10, 25, 50, 100]} />}
-      filters={
-        isMobile
-          ? [
-              <SearchInput
-                source="q"
-                alwaysOn
-                className="w-full"
-                placeholder="Name, sector"
-              />,
-            ]
-          : undefined
-      }
     >
       <CompanyListLayout />
-      <Button
-        variant="default"
-        size="icon"
-        className="rounded-full fixed bottom-12 right-12 w-12 h-12"
-        asChild
-      >
-        <Link to="/companies/create">
-          <span className="sr-only">Create new company</span>
-          <Plus />
-        </Link>
-      </Button>
     </List>
   );
 };
