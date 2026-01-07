d12860eeb6942d7ff730d657eaa121717c50f6d0
Jean-Baptiste Kaiser <jb@marmelab.com>
2026-01-07 17:13:48 +0100

    add mobile navigation

--- src/components/atomic-crm/layout/MobileLayout.tsx
+++ src/components/atomic-crm/layout/MobileLayout.tsx
@@ -5,17 +5,19 @@ import { Error } from "@/components/admin/error";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import MobileHeader from "./MobileHeader";
+import { MobileNavigation } from "./MobileNavigation";
 
 export const MobileLayout = ({ children }: { children: ReactNode }) => (
   <>
     <MobileHeader />
-    <main className="max-w-screen-xl mx-auto pt-4 px-4" id="main-content">
+    <main className="max-w-screen-xl mx-auto pt-4 px-4 pb-18" id="main-content">
       <ErrorBoundary FallbackComponent={Error}>
         <Suspense fallback={<Skeleton className="h-12 w-12 rounded-full" />}>
           {children}
         </Suspense>
       </ErrorBoundary>
     </main>
+    <MobileNavigation />
     <Notification />
   </>
 );
