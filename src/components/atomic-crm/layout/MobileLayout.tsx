6e8cae3f7a18b418385a71918b3abb20c6e1ec23
Jean-Baptiste Kaiser <jb@marmelab.com>
2026-01-07 11:40:22 +0100

    add mobile header

---
+++ src/components/atomic-crm/layout/MobileLayout.tsx
@@ -0,0 +1,21 @@
+import { Suspense, type ReactNode } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Notification } from "@/components/admin/notification";
+import { Error } from "@/components/admin/error";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import MobileHeader from "./MobileHeader";
+
+export const MobileLayout = ({ children }: { children: ReactNode }) => (
+  <>
+    <MobileHeader />
+    <main className="max-w-screen-xl mx-auto pt-4 px-4" id="main-content">
+      <ErrorBoundary FallbackComponent={Error}>
+        <Suspense fallback={<Skeleton className="h-12 w-12 rounded-full" />}>
+          {children}
+        </Suspense>
+      </ErrorBoundary>
+    </main>
+    <Notification />
+  </>
+);
