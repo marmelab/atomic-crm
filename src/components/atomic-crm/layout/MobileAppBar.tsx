0711b8caf11d350eba02e2c39010573f26c0eb4c
Jean-Baptiste Kaiser <jb@marmelab.com>
2026-01-13 13:42:44 +0100

    use a portal in header

---
+++ src/components/atomic-crm/layout/MobileAppBar.tsx
@@ -0,0 +1,12 @@
+export const MobileAppBar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-10 bg-secondary h-14 px-4 w-full flex justify-between items-center">
+      <div
+        id="atomic-crm-header"
+        className="w-full flex justify-between items-center"
+      >
+        {/* Portal content will be rendered here */}
+      </div>
+    </header>
+  );
+};
