98d1a3103f38b02685508eb87543df381d7f9b83
React-Admin CI <react-admin-ee@marmelab.com>
2026-01-21 15:40:02 +0100

    Remove json import from onboarding

--- src/components/atomic-crm/dashboard/DashboardStepper.tsx
+++ src/components/atomic-crm/dashboard/DashboardStepper.tsx
@@ -8,7 +8,6 @@ import { Card, CardContent } from "@/components/ui/card";
 
 import { ContactImportButton } from "../contacts/ContactImportButton";
 import useAppBarHeight from "../misc/useAppBarHeight";
-import { ImportFromJsonButton } from "../misc/ImportFromJsonButton";
 
 export const DashboardStepper = ({
   step,
@@ -52,7 +51,6 @@ export const DashboardStepper = ({
                 <div className="flex gap-8">
                   <CreateButton label="New Contact" resource="contacts" />
                   <ContactImportButton />
-                  <ImportFromJsonButton />
                 </div>
               </div>
             </div>
