d1140d112582795d4ab0d86679947a5a8196b306
fzaninotto <fzaninotto@gmail.com>
2026-01-28 14:38:35 +0100

    Fix date formatting

--- src/components/atomic-crm/misc/RelativeDate.tsx
+++ src/components/atomic-crm/misc/RelativeDate.tsx
@@ -1,5 +1,12 @@
-import { formatRelative } from "date-fns";
+import { differenceInDays, formatRelative } from "date-fns";
 
 export function RelativeDate({ date }: { date: string }) {
-  return formatRelative(new Date(date), new Date());
+  const dateObj = new Date(date);
+  const now = new Date();
+
+  if (differenceInDays(now, dateObj) > 6) {
+    return dateObj.toLocaleDateString();
+  }
+
+  return formatRelative(dateObj, now);
 }
