f61c0bca9989f70d7d958300c8bb3360b91cfe3d
fzaninotto <fzaninotto@gmail.com>
2026-01-28 18:18:39 +0100

    Turn note summaries in the activity log into links to the full note

--- src/components/atomic-crm/activity/ActivityLogNote.tsx
+++ src/components/atomic-crm/activity/ActivityLogNote.tsx
@@ -1,16 +1,15 @@
 import { type ReactNode } from "react";
+import { Link } from "react-router";
 
 import { Markdown } from "../misc/Markdown";
 
-type ActivityLogContactNoteCreatedProps = {
+type ActivityLogNoteProps = {
   header: ReactNode;
   text: string;
+  link: string;
 };
 
-export function ActivityLogNote({
-  header,
-  text,
-}: ActivityLogContactNoteCreatedProps) {
+export function ActivityLogNote({ header, text, link }: ActivityLogNoteProps) {
   if (!text) {
     return null;
   }
@@ -21,11 +20,11 @@ export function ActivityLogNote({
         <div className="flex flex-row space-x-1 items-center w-full">
           {header}
         </div>
-        <div>
+        <Link to={link} className="hover:bg-muted rounded transition-colors">
           <Markdown className="text-sm line-clamp-3 overflow-hidden [&_p]:my-1 [&_p:first-child]:mt-0 [&_blockquote]:border-l-2 [&_blockquote]:pl-3 [&_blockquote]:my-1 [&_blockquote]:text-muted-foreground [&_a]:text-primary [&_a]:underline [&_a:hover]:no-underline">
             {text}
           </Markdown>
-        </div>
+        </Link>
       </div>
     </div>
   );
