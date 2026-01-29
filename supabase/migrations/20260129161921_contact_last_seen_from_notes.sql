170f4f5f91b4f707dc3c5aadf613a0beb6c4ac05
React-Admin CI <react-admin-ee@marmelab.com>
2026-01-29 17:42:51 +0100

    Fix import

--- supabase/migrations/20260129161921_contact_last_seen_from_notes.sql
+++ supabase/migrations/20260129161921_contact_last_seen_from_notes.sql
@@ -7,12 +7,10 @@ CREATE OR REPLACE FUNCTION public.handle_contact_note_created_or_updated()
  SET search_path TO ''
 AS $function$
 begin
-  update public.contacts set public.contacts.last_seen = new.updated_at where public.contacts.id = new.date;
+  update public.contacts set public.contacts.last_seen = new.date where public.contacts.id = new.contact_id;
   return new;
 end;
 $function$
 ;
 
 CREATE TRIGGER on_public_contact_notes_created_or_updated AFTER INSERT ON public.contact_notes FOR EACH ROW EXECUTE FUNCTION public.handle_contact_note_created_or_updated();
-
-
