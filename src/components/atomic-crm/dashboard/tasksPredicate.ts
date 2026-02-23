e2753dc538869ba0db50ded0c500a1ce03cb1545
ThieryMichel <thiery@marmelab.com>
2026-02-23 11:09:51 +0100

    extract taskPredicate and test them

---
+++ src/components/atomic-crm/dashboard/tasksPredicate.ts
@@ -0,0 +1,56 @@
+import { startOfToday } from "date-fns/startOfToday";
+import { endOfToday } from "date-fns/endOfToday";
+import { endOfTomorrow } from "date-fns/endOfTomorrow";
+import { endOfWeek } from "date-fns/endOfWeek";
+
+import { getDay } from "date-fns";
+import type { Identifier } from "ra-core";
+
+const today = new Date();
+const todayDayOfWeek = getDay(today);
+export const isBeforeFriday = todayDayOfWeek < 5; // Friday is represented by 5
+const startOfTodayDate = startOfToday();
+const endOfTodayDate = endOfToday();
+const endOfTomorrowDate = endOfTomorrow();
+const endOfWeekDate = endOfWeek(today, { weekStartsOn: 0 });
+
+type Task = {
+  id: Identifier;
+  due_date: string;
+  done_date: string | null;
+};
+
+export const isOverdue = (task: Task) =>
+  task.done_date == null || new Date(task.due_date) < startOfTodayDate;
+
+export const isDueToday = (task: Task) => {
+  if (task.done_date != null) {
+    return false;
+  }
+  const dueDate = new Date(task.due_date);
+  return dueDate >= startOfTodayDate && dueDate < endOfTodayDate;
+};
+
+export const isDueTomorrow = (task: Task) => {
+  if (task.done_date != null) {
+    return false;
+  }
+  const dueDate = new Date(task.due_date);
+  return dueDate >= endOfTodayDate && dueDate < endOfTomorrowDate;
+};
+
+export const isDueThisWeek = (task: Task) => {
+  if (task.done_date != null) {
+    return false;
+  }
+  const dueDate = new Date(task.due_date);
+  return dueDate >= endOfTomorrowDate && dueDate < endOfWeekDate;
+};
+
+export const isDueLater = (task: Task) => {
+  if (task.done_date != null) {
+    return false;
+  }
+  const dueDate = new Date(task.due_date);
+  return dueDate >= endOfWeekDate;
+};
