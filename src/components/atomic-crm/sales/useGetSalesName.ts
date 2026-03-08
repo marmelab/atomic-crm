04a52800c876e94645acdfc3b954110d79e22aab
François Zaninotto <fzaninotto@gmail.com>
2026-03-08 19:48:29 +0100

    Fix formatting

--- src/components/atomic-crm/sales/useGetSalesName.ts
+++ src/components/atomic-crm/sales/useGetSalesName.ts
@@ -1,8 +1,19 @@
 import type { Identifier } from "ra-core";
 import { useGetManyAggregate } from "ra-core";
 
-export const useGetSalesName = (id?: Identifier, options?: { enabled?: boolean }) => {
+export const useGetSalesName = (
+  id?: Identifier,
+  options?: { enabled?: boolean },
+) => {
   const enabled = options?.enabled ?? id != null;
-  const { data, error } = useGetManyAggregate("sales", { ids: id !== null ? [id]: undefined }, { enabled });
-  return  data ? `${data[0].first_name} ${data[0].last_name}` : error ? "??" : "";
-}
+  const { data, error } = useGetManyAggregate(
+    "sales",
+    { ids: id !== null ? [id] : undefined },
+    { enabled },
+  );
+  return data
+    ? `${data[0].first_name} ${data[0].last_name}`
+    : error
+      ? "??"
+      : "";
+};
