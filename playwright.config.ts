0456ad6d319810bc69cefa3a10e8ca3fa87c49f3
ThieryMichel <thiery@marmelab.com>
2026-03-11 14:56:46 +0100

    update onboarding test to handle mobile ui too

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,14 +53,14 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
 
     /* Test against branded browsers. */
     // {
