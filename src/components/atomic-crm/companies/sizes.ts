08ea0dca7677c14d9d58d57a561bb45c1ddfceec
adrian.mularczyk <adrian.mularczyk@gmail.com>
2026-03-11 09:52:20 +0100

    changed sizes

--- src/components/atomic-crm/companies/sizes.ts
+++ src/components/atomic-crm/companies/sizes.ts
@@ -1,7 +1,7 @@
 export const sizes = [
-  { id: 1, name: "1 pracownik" },
-  { id: 10, name: "2-9 pracowników" },
-  { id: 50, name: "10-49 pracowników" },
-  { id: 250, name: "50-249 pracowników" },
-  { id: 500, name: "250 lub więcej pracowników" },
+  { id: 1, name: "Mikro" },
+  { id: 10, name: "Mały" },
+  { id: 50, name: "Średni" },
+  { id: 250, name: "Duży" },
+  // { id: 500, name: "250 lub więcej pracowników" },
 ];
