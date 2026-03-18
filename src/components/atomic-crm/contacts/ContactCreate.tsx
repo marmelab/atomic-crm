ef52fda83db798032a7bd64468efd2cdca7fec83
ThieryMichel <thiery@marmelab.com>
2026-03-18 16:06:01 +0100

    add integration tests on ContactCreate

--- src/components/atomic-crm/contacts/ContactCreate.tsx
+++ src/components/atomic-crm/contacts/ContactCreate.tsx
@@ -1,22 +1,34 @@
-import { CreateBase, Form, useGetIdentity } from "ra-core";
+import { CreateBase, Form, useGetIdentity, type MutationMode } from "ra-core";
 import { Card, CardContent } from "@/components/ui/card";
 
 import { ContactInputs } from "./ContactInputs";
 import { FormToolbar } from "../layout/FormToolbar";
-import { cleanupContactForCreate, defaultEmailJsonb } from "./cleanupContact";
+import {
+  cleanupContactForCreate,
+  defaultEmailJsonb,
+  defaultPhoneJsonb,
+} from "./cleanupContact";
 
-export const ContactCreate = () => {
+export const ContactCreate = ({
+  mutationMode,
+}: {
+  mutationMode?: MutationMode;
+}) => {
   const { identity } = useGetIdentity();
 
   return (
-    <CreateBase redirect="show" transform={cleanupContactForCreate}>
+    <CreateBase
+      redirect="show"
+      transform={cleanupContactForCreate}
+      mutationMode={mutationMode}
+    >
       <div className="mt-2 flex lg:mr-72">
         <div className="flex-1">
           <Form
             defaultValues={{
               sales_id: identity?.id,
               email_jsonb: defaultEmailJsonb,
-              phone_jsonb: defaultEmailJsonb,
+              phone_jsonb: defaultPhoneJsonb,
             }}
           >
             <Card>
