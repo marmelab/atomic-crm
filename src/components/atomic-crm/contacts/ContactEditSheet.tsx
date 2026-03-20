d8bc38cff1954e190f9e601c6a6f5b71c591b328
ThieryMichel <thiery@marmelab.com>
2026-03-20 14:54:31 +0100

    add default values for mobile

--- src/components/atomic-crm/contacts/ContactEditSheet.tsx
+++ src/components/atomic-crm/contacts/ContactEditSheet.tsx
@@ -1,7 +1,11 @@
 import type { Identifier } from "ra-core";
 import { EditSheet } from "../misc/EditSheet";
 import { ContactInputs } from "./ContactInputs";
-import { cleanupContactForEdit } from "./contactModel";
+import {
+  cleanupContactForEdit,
+  defaultEmailJsonb,
+  defaultPhoneJsonb,
+} from "./contactModel";
 
 export interface ContactEditSheetProps {
   open: boolean;
@@ -21,6 +25,10 @@ export const ContactEditSheet = ({
       open={open}
       onOpenChange={onOpenChange}
       transform={cleanupContactForEdit}
+      defaultValues={{
+        email_jsonb: defaultEmailJsonb,
+        phone_jsonb: defaultPhoneJsonb,
+      }}
     >
       <ContactInputs />
     </EditSheet>
