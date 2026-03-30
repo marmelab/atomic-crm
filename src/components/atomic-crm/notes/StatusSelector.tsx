d3b0670696d3a2593a3490a92ad9de7e08f3ecd1
fzaninotto <fzaninotto@gmail.com>
2026-03-30 17:48:39 +0200

    Add the ability to use null value

--- src/components/atomic-crm/notes/StatusSelector.tsx
+++ src/components/atomic-crm/notes/StatusSelector.tsx
@@ -10,6 +10,8 @@ import {
 import { Status } from "../misc/Status";
 import { useConfigurationContext } from "../root/ConfigurationContext";
 
+const NONE_VALUE = "__none__";
+
 type StatusSelectorProps = {
   disabled?: boolean;
   status?: string;
@@ -27,19 +29,34 @@ export const StatusSelector = ({
 
   const currentStatus = noteStatuses.find((s) => s.value === status);
 
+  /**
+   * Radix's Select component doesn't allow empty string as value, so we use a placeholder value and convert it back to empty string on change
+   * @see https://github.com/radix-ui/primitives/issues/2706
+   */
+  const handleValueChange = (value: string) => {
+    setStatus(value === NONE_VALUE ? "" : value);
+  };
+
   return (
-    <Select disabled={disabled} value={status} onValueChange={setStatus}>
+    <Select
+      disabled={disabled}
+      value={status || NONE_VALUE}
+      onValueChange={handleValueChange}
+    >
       <SelectTrigger className={cn("w-32", triggerClassName)}>
         <SelectValue>
-          {currentStatus && (
+          {currentStatus ? (
             <div className="flex items-center gap-2">
               <Status status={currentStatus.value} />
               {currentStatus.label}
             </div>
+          ) : (
+            "None"
           )}
         </SelectValue>
       </SelectTrigger>
       <SelectContent>
+        <SelectItem value={NONE_VALUE}>None</SelectItem>
         {noteStatuses.map((statusOption) => (
           <SelectItem key={statusOption.value} value={statusOption.value}>
             <div className="flex items-center gap-2">
